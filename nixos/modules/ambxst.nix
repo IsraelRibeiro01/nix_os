7555d5f138a6284f8df5ea15a64a4c6ec76b0de6
luwiblu <luwiblu@gmail.com>
2026-01-23 23:02:52 -0300

    ambxst to unstable pkgs

--- nixos/modules/ambxst.nix
+++ nixos/modules/ambxst.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, inputs, ... }:
+{ config, pkgs, ambxst,unstable, ... }:
 
 {
   environment.systemPackages = with pkgs; [
     # using the ambxst package from flake input
-    inputs.ambxst.packages.${pkgs.system}.default
+    ambxst.packages.${pkgs.system}.default
   ];
 
   # if Ambxst needs service-like files or to be started automatically,
